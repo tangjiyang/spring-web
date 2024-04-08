98996a2da688f3a77b656ce5189b4c705af7a550
tangjiyang <2569191826@qq.com>
2024-04-08 14:04:25 +0800

    initial

---
+++ src/main/java/com/tang/test/web/SpringWeb9001.java
@@ -0,0 +1,12 @@
+package com.tang.test.web;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWeb9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWeb9001.class, args);
+    }
+}
