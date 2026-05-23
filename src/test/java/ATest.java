4379faac64d2d9be6bf0f412614b4ea91762c54a
tangjiyang <2569191826@qq.com>
2026-05-23 18:28:40 +0800

    '1';

---
+++ src/test/java/ATest.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ATest {
+
+    @Test
+    public void testA() {
+
+        Assertions.assertTrue(true);
+    }
+}
