98e91dd6adfc49fcd96016175bb7adcd007fa462
tangjiyang <2569191826@qq.com>
2026-05-24 19:02:08 +0800

    '1'

---
+++ src/test/java/com/tang/test/web/IndexControllerTest.java
@@ -0,0 +1,25 @@
+package com.tang.test.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.client.RestTestClient;
+
+@SpringBootTest
+@AutoConfigureRestTestClient
+public class IndexControllerTest {
+    @Autowired
+    private RestTestClient restTestClient;
+
+    @Test
+    public void testIndex() {
+        byte[] responseBodyContent = restTestClient.get()
+                .uri("/index")
+                .exchange()
+                .expectStatus().isOk()
+                .returnResult().getResponseBodyContent();
+        System.out.println(new String(responseBodyContent));
+    }
+
+}
